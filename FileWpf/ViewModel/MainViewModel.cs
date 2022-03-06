6efb45bfd250b8b81e55f0bee4a066af7c523a1d
PiotrKroczak <piotr.kroczak@gmail.com>
2022-03-06 22:02:26 +0100

    Start

---
+++ FileWpf/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWpf.ViewModel
+{
+    public class MainViewModel
+    {
+        #region Constructors - Konstruktory
+        public MainViewModel()
+        { 
+        
+        }
+        #endregion
+        #region Properties - Wlasciwosci
+
+        #endregion
+        #region Methods - Metody 
+
+        #endregion
+}
+}
